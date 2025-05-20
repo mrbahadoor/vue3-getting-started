2cdfce5edce9b3ae7e12a375693fd6beb230dbc9
Rushdi Bahadoor <my_name@example.com>
2025-05-20 22:39:51 +0400

    1-installation

---
+++ my-vue-app/src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+const msg = ref('Hello world!')
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <input v-model="msg"/>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
